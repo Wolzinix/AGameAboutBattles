4edfe5251543e2f0fc70dadaf47f49e4d64c6fbf
Wolzinix <wolzinix@outlook.fr>
2025-12-22 16:52:04 -0500

    feat: Add variable to move

--- Assets/Script/Entity/MovementEntity.cs
+++ Assets/Script/Entity/MovementEntity.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public class MovementEntity : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private float speed = 1;
+    public bool IsMoving = true; 
     void Start()
     {
         StartCoroutine(MoveForward());
@@ -13,7 +15,7 @@ public class MovementEntity : MonoBehaviour
     IEnumerator MoveForward()
     {
         yield return null;
-        while(true)
+        while(IsMoving)
         {
             yield return new WaitForSeconds(0.1f * speed);
             transform.position += new Vector3(0, 0, 5f) * Time.deltaTime;
